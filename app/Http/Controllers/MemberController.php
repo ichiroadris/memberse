97936c700d7b415cc5f01517b0c14884005d086f
ichiro <ichiroadris@gmail.com>
2020-11-18 20:20:02 +0800

    member table

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class MemberController extends Controller
 {
     public function index() {
-        return view('welcome');
+        return Inertia::render('Admin/Member/Index');
+
     }
 }
